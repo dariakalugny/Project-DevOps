42257598d547cdbb05af3ef57a7990b23e28547b
daria <dariakalugny2405@gmail.com>
2023-08-23 21:17:11 +0300

    first commit

---
+++ Terraform/varible.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type        = eu-west-1
+  description = "AWS region"
+}
+
+variable "region" {
+  type        = eu-west-1
+  description = "AWS region"
+}
+
+variable "region" {
+  type        = eu-west-1
+  description = "AWS region"
+}
+
+variable "region" {
+  type        = eu-west-1
+  description = "AWS region"
+}
+
