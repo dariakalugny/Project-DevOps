a02869acd1ddf649b840caf2519faa40d92b3321
daria <dariakalugny2405@gmail.com>
2023-09-18 16:45:31 +0300

    first commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -15,25 +15,22 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_s3_bucket" "default" {
-  bucket = "tftest.applicationversion.bucket"
+
+resource "aws_elastic_beanstalk_application" "my_app" {
+  name = "2048"
 }
 
-resource "aws_s3_object" "default" {
-  bucket = aws_s3_bucket.default.id
-  key    = "2048/Dockerfile"
-  source = "Dockerfile"
+resource "aws_elastic_beanstalk_environment" "my_env" {
+  name                = "2048-game-terraform"
+  application         = aws_elastic_beanstalk_application.my_app.name
+  solution_stack_name = "64bit Amazon Linux 2 v4.5.2 running Python 3.8"
+  // Add other environment settings as needed
 }
 
-resource "aws_elastic_beanstalk_application" "default" {
-  name        = "2048-game-terraform"
-  description = "2048-game-terraform-daria-shani"
-}
-
-resource "aws_elastic_beanstalk_application_version" "default" {
-  name        = "tf-test-version-label"
-  application = "tf-test-name"
-  description = "application version created by terraform"
-  bucket      = aws_s3_bucket.default.id
-  key         = master.zip
+resource "aws_elastic_beanstalk_source_bundle" "my_app_source" {
+  application_name = aws_elastic_beanstalk_application.my_app.name
+  source_bundle {
+    s3_bucket = "project-daria-shani"
+    s3_key    = "s3://project-daria-shani/2048/Dockerfile"
+  }
 }
