9294f3cc18c9f943a38bea4558969267191e816e
daria <dariakalugny2405@gmail.com>
2023-09-18 19:25:13 +0300

    first commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,31 +1,10 @@
-
-provider "aws" {
-  region = var.aws_region
+resource "aws_elastic_beanstalk_application" "tftest" {
+  name        = "tf-test-name"
+  description = "tf-test-desc"
 }
 
-resource "aws_elastic_beanstalk_application" "my_app" {
-  name        = var.eb_application_name
-  description = "My Elastic Beanstalk Application"
-}
-
-resource "aws_elastic_beanstalk_environment" "my_env" {
-  name                = var.eb_environment_name
-  application         = aws_elastic_beanstalk_application.my_app.name
-  solution_stack_name = var.eb_solution_stack
-  # Add other environment settings as needed
-}
-
-resource "aws_s3_bucket" "my_app_bucket" {
-  bucket = var.s3_bucket_name
-  acl    = "private"
-}
-
-# Deploy the application to Elastic Beanstalk environment
-resource "aws_elastic_beanstalk_application_version" "app_version" {
-  name        = "my-app-version"
-  description = "My Application Version"
-  application = aws_elastic_beanstalk_application.my_app.name
-
-  s3_bucket = aws_s3_bucket.my_app_bucket.id
-  s3_key    = "path/to/your/application.zip"
+resource "aws_elastic_beanstalk_environment" "tfenvtest" {
+  name                = "tf-test-name"
+  application         = aws_elastic_beanstalk_application.tftest.name
+  solution_stack_name = "64bit Amazon Linux 2015.03 v2.0.3 running Go 1.4"
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,27 +1,3 @@
-variable "aws_region" {
-   # AWS Region
-   aws_region = "us-west-1"
-}
-
-
-variable "eb_environment_name" {
-   # Elastic Beanstalk Environment Name
-   eb_environment_name = "2048-project-daria-shani"
-}
-
-
-variable "eb_solution_stack" {
-  # Elastic Beanstalk Solution Stack Name
-    eb_solution_stack = "docker"
-}
-
-
-variable "s3_bucket_name" {
-  # S3 Bucket Name for Application Deployment
-    s3_bucket_name = "project-daria-shani"
-}
-
-
 
 
 
