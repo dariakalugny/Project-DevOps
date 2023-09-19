8a85eceadb3bf580f6abac6be556302e13b1ce24
daria <dariakalugny2405@gmail.com>
2023-09-19 15:37:06 +0300

    first commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -26,7 +26,7 @@ resource "aws_elastic_beanstalk_application" "test-2048" {
 resource "aws_elastic_beanstalk_environment" "Application2048-daria-shani" {
   name                = "test-2048"
   application         = aws_elastic_beanstalk_application.test-2048.name
-  solution_stack_name = "64bit Amazon Linux 2023 v4.0.1 running Docker"
+  solution_stack_name = "Docker running on 64bit Amazon Linux 2023"
 
 
 
